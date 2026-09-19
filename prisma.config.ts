029b41cd0803d4af8552a9bb3a04f9e12556f3a3
Nguyễn Hồng Khánh <hongkhanh2311@gmail.com>
2026-09-19 18:03:04 +0700

    chore(database): initialize prisma setup and docker compose

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { definePrismaConfig } from "prisma/config";
+
+export default definePrismaConfig({
+  schema: "prisma/schema.prisma", 
+  datasource: {
+    url: process.env.DATABASE_URL, 
+  },
+});
